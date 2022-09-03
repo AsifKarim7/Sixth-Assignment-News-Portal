d0639d7db35de5446e91bdb17a2b5ccf5e134d5e
AsifKarim7 <asifkarim275@gmail.com>
2022-09-03 18:48:09 +0600

    toggle spinner & not found added

--- js/app.js
+++ js/app.js
@@ -26,44 +26,63 @@ const getNews = (News) => {
     .then(data => displayNews(data.data));
 }
 
-const displayNews = news => {
-  const newsContainer = document.getElementById('news-container');
-  newsContainer.innerHTML = ``;
-  news.forEach(showNews => {
-    const newsDiv = document.createElement('div');
-    newsDiv.classList.add('row', 'border', 'rounded', 'mb-4', 'p-4');
-
-    newsDiv.innerHTML = `
-        
-                  <div class="col-md-3">
-                    <img src="${showNews.thumbnail_url}" class="news-img rounded" alt="image">
-                  </div>
-
-                  <div class="col-md-9">
-                    <div class="card-body">
-                      <h5 class="card-title fw-bolder pb-4">${showNews.title}</h5>
-                      <p class="card-text text-secondary pb-3">${showNews.details.slice(0, 200)}</p>
-                    
-                      <div class = "d-flex justify-content-between pt-5">
-                      <div class= "d-flex ">
-                    <img  src="${showNews.author.img}" class=" author-img" alt="image">
-
-                    <p class="card-text px-2"><small class="fw-bold">${showNews.author.name ? showNews.author.name : 'Unknown'}</small></p>
-                    </div>
-
-                    <div class= "d-flex">
-                    <i class="bi bi-eye-fill px-2"></i>
-                    <p class="card-text"><small class="fw-bold">${showNews.total_view ? showNews.total_view : 'no views'}</small></p>
-                    </div>
-
-                    <button onclick="loadNewsdetails()" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#newsDetailModal"><small>Show Details</small></button>
-                    
-                      </div>
-                    </div>
-                  </div>
-            
-        `;
-    newsContainer.appendChild(newsDiv);
-  })
+const toggleSpinner = isLoading => {
+  const loadingSection = document.getElementById('loader');
+  if (isLoading) {
+    loadingSection.classList.remove('d-none')
+  }
+  else {
+    loadingSection.classList.add('d-none');
+  }
 }
 
+const displayNews = news => {
+  toggleSpinner(true);
+  if (news.length == 0) {
+    toggleSpinner(false);
+    const error = document.getElementById('error');
+    error.innerText = " Data not Found."
+  }
+  else {
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.innerHTML = ``;
+    news.forEach(showNews => {
+      const newsDiv = document.createElement('div');
+      newsDiv.classList.add('row', 'border', 'rounded', 'mb-4', 'p-4');
+
+      newsDiv.innerHTML = `
+          
+                    <div class="col-md-3">
+                      <img src="${showNews.thumbnail_url}" class="news-img rounded" alt="image">
+                    </div>
+  
+                    <div class="col-md-9">
+                      <div class="card-body">
+                        <h5 class="card-title fw-bolder pb-4">${showNews.title}</h5>
+                        <p class="card-text text-secondary pb-3">${showNews.details.slice(0, 200)}</p>
+                      
+                        <div class = "d-flex justify-content-between pt-5">
+                        <div class= "d-flex ">
+                      <img  src="${showNews.author.img}" class=" author-img" alt="image">
+  
+                      <p class="card-text px-2"><small class="fw-bold">${showNews.author.name ? showNews.author.name : 'Not Found'}</small></p>
+                      </div>
+  
+                      <div class= "d-flex">
+                      <i class="bi bi-eye-fill px-2"></i>
+                      <p class="card-text"><small class="fw-bold">${showNews.total_view ? showNews.total_view : 'no views'}</small></p>
+                      </div>
+  
+                      <button onclick="loadNewsDetails('${showNews._id}')" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#newsDetailModal"><small>Show Details</small></button>
+                      
+                        </div>
+                      </div>
+                    </div>
+              
+          `;
+      newsContainer.appendChild(newsDiv);
+      toggleSpinner(false);
+    })
+  }
+
+}
